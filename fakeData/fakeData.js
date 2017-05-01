c058c7af7a80141618235b1f0a6cab7788cf6eda
Kevin Nguyen <knguy47@gmail.com>
2017-05-01 14:52:17 -0500

    Added Fake Twitch Data

---
+++ fakeData/fakeData.js
@@ -0,0 +1,226 @@
+var fakeData = {
+  "featured": [
+    {
+      "text": "<p><a href=\"/zizaran\">Ziz</a> is an avid hardcore Path of Exile player. His energy and furious passion drives his stream. He is known for never quitting and always focusing on the positive sides of life, gaming and people. Watch for competitive game play, with a strong focus on interaction with you — the viewer!</p>\n\n<p><a href=\"partnerships,recurring\"></a></p>\n",
+      "title": "Path of Exile with Zizaran",
+      "sponsored": false,
+      "priority": 5,
+      "scheduled": true,
+      "image": "https://s.jtvnw.net/jtv_user_pictures/hosted_images/zizaran_fp_april17",
+      "stream": {
+        "game": "Path of Exile",
+        "viewers": 3502,
+        "video_height": 720,
+        "average_fps": 30.8802308802,
+        "_id": "25182025200",
+        "delay": 0,
+        "created_at": "2017-05-01T14:02:56Z",
+        "is_playlist": false,
+        "preview": {
+          "small": "https://static-cdn.jtvnw.net/previews-ttv/live_user_zizaran-80x45.jpg",
+          "medium": "https://static-cdn.jtvnw.net/previews-ttv/live_user_zizaran-320x180.jpg",
+          "large": "https://static-cdn.jtvnw.net/previews-ttv/live_user_zizaran-640x360.jpg",
+          "template": "https://static-cdn.jtvnw.net/previews-ttv/live_user_zizaran-{width}x{height}.jpg"
+        },
+        "channel": {
+          "mature": false,
+          "status": "Ziz - Alkaizer AMA Live Now! ( ＾◡＾)っ !pnp !filter !starter !trade !build !rip",
+          "broadcaster_language": "en",
+          "display_name": "Zizaran",
+          "game": "Path of Exile",
+          "language": "en",
+          "name": "zizaran",
+          "created_at": "2012-09-27T13:28:56Z",
+          "updated_at": "2017-05-01T19:35:49Z",
+          "_id": "36483360",
+          "logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/zizaran-profile_image-d534b9791c469a48-300x300.png",
+          "video_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/zizaran-channel_offline_image-f9aa6d94a452cb3f-1920x1080.jpeg",
+          "profile_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/zizaran-profile_banner-1e6d3da3d0630c54-480.jpeg",
+          "profile_banner_background_color": null,
+          "partner": true,
+          "url": "https://www.twitch.tv/zizaran",
+          "views": 4930817,
+          "followers": 47309
+        }
+      }
+    },
+    {
+      "text": "<p>13 regions will send their Spring Split champion to face off in Brazil from 4/28 - 5/21, but only One Will Reign.</p>\n\n<br>\n\n\n<p><a href=\"/riotgames\">Click here</a> to watch and chat!</p>\n\n<p><a href=\"esports,tournament\"></a></p>\n",
+      "title": "2017 League Of Legends Mid-Season Invitational",
+      "sponsored": false,
+      "priority": 5,
+      "scheduled": true,
+      "image": "https://s.jtvnw.net/jtv_user_pictures/hosted_images/riotgames_fp_msi2017",
+      "stream": {
+        "game": "League of Legends",
+        "viewers": 56639,
+        "video_height": 1080,
+        "average_fps": 60.8375778155,
+        "_id": "25183086048",
+        "delay": 0,
+        "created_at": "2017-05-01T16:59:11Z",
+        "is_playlist": false,
+        "preview": {
+          "small": "https://static-cdn.jtvnw.net/previews-ttv/live_user_riotgames-80x45.jpg",
+          "medium": "https://static-cdn.jtvnw.net/previews-ttv/live_user_riotgames-320x180.jpg",
+          "large": "https://static-cdn.jtvnw.net/previews-ttv/live_user_riotgames-640x360.jpg",
+          "template": "https://static-cdn.jtvnw.net/previews-ttv/live_user_riotgames-{width}x{height}.jpg"
+        },
+        "channel": {
+          "mature": false,
+          "status": "MSI 2017: Play-In Day 4",
+          "broadcaster_language": "en",
+          "display_name": "Riot Games",
+          "game": "League of Legends",
+          "language": "en",
+          "name": "riotgames",
+          "created_at": "2012-09-08T06:53:10Z",
+          "updated_at": "2017-05-01T19:36:17Z",
+          "_id": "36029255",
+          "logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/riotgames-profile_image-4be3ad99629ac9ba-300x300.jpeg",
+          "video_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/riotgames-channel_offline_image-ff7e9d25134432f3-1920x1080.png",
+          "profile_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/riotgames-profile_banner-1704e35b8cffbe65-480.png",
+          "profile_banner_background_color": null,
+          "partner": true,
+          "url": "https://www.twitch.tv/riotgames",
+          "views": 892612790,
+          "followers": 2332200
+        }
+      }
+    },
+    {
+      "text": "<p>The Escapist is devoted to video games and geek culture. Our editors and pundits bring their hilarious brand of criticism to the things we all love—while never forgetting why we love them! Whether you want to play games, learn the latest news, or get the most brutally honest reviews on the web, you can do it here.</p>\n\n<br>\n\n\n<p><a href=\"/escapistmagazine\">Watch here</a> or check out escapistmagazine.com each day for your escape from the everyday.</p>\n\n<p><a href=\"partnerships,%20showrecurring\"></a></p>\n",
+      "title": "Escapist Magazine",
+      "sponsored": false,
+      "priority": 5,
+      "scheduled": true,
+      "image": "https://s.jtvnw.net/jtv_user_pictures/hosted_images/escapistmagazine_fp_march2016.jpg",
+      "stream": {
+        "game": "Chroma Squad",
+        "viewers": 442,
+        "video_height": 720,
+        "average_fps": 30,
+        "_id": "25183966544",
+        "delay": 0,
+        "created_at": "2017-05-01T19:00:11Z",
+        "is_playlist": false,
+        "preview": {
+          "small": "https://static-cdn.jtvnw.net/previews-ttv/live_user_escapistmagazine-80x45.jpg",
+          "medium": "https://static-cdn.jtvnw.net/previews-ttv/live_user_escapistmagazine-320x180.jpg",
+          "large": "https://static-cdn.jtvnw.net/previews-ttv/live_user_escapistmagazine-640x360.jpg",
+          "template": "https://static-cdn.jtvnw.net/previews-ttv/live_user_escapistmagazine-{width}x{height}.jpg"
+        },
+        "channel": {
+          "mature": true,
+          "status": "Let's try Chroma Squad!",
+          "broadcaster_language": "en",
+          "display_name": "EscapistMagazine",
+          "game": "Chroma Squad",
+          "language": "en",
+          "name": "escapistmagazine",
+          "created_at": "2013-02-04T22:20:41Z",
+          "updated_at": "2017-05-01T19:36:36Z",
+          "_id": "40040560",
+          "logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/escapistmagazine-profile_image-2b402c5bb208ceec-300x300.png",
+          "video_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/escapistmagazine-channel_offline_image-f10ca349e6af25b9-1920x1080.jpeg",
+          "profile_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/escapistmagazine-profile_banner-e12bdb9633338f5b-480.png",
+          "profile_banner_background_color": "#000000",
+          "partner": true,
+          "url": "https://www.twitch.tv/escapistmagazine",
+          "views": 13747498,
+          "followers": 17873
+        }
+      }
+    },
+    {
+      "text": "<p>There are some amazing smaller broadcasters on Twitch, and we want these rising stars to have an opportunity to showcase what they're all about. That's where the Twitch Partner Spotlight comes in. Every week we choose an up-and-coming broadcaster for some guaranteed front page and social media exposure and help them share their talents with a wider audience.</p>\n\n<br>\n\n\n<p>Come watch this week's pick—<a href=\"/CMDRHughMann\">CMDRHughMann</a>!</p>\n\n<p><a href=\"twitchweekly,spotlight\"></a></p>\n",
+      "title": "Twitch Partner Spotlight",
+      "sponsored": false,
+      "priority": 7,
+      "scheduled": true,
+      "image": "https://s.jtvnw.net/jtv_user_pictures/hosted_images/TwitchPartnerSpotlight.png",
+      "stream": {
+        "game": "Grand Theft Auto V",
+        "viewers": 186,
+        "video_height": 720,
+        "average_fps": 60,
+        "_id": "25181677024",
+        "delay": 0,
+        "created_at": "2017-05-01T13:00:06Z",
+        "is_playlist": false,
+        "preview": {
+          "small": "https://static-cdn.jtvnw.net/previews-ttv/live_user_cmdrhughmann-80x45.jpg",
+          "medium": "https://static-cdn.jtvnw.net/previews-ttv/live_user_cmdrhughmann-320x180.jpg",
+          "large": "https://static-cdn.jtvnw.net/previews-ttv/live_user_cmdrhughmann-640x360.jpg",
+          "template": "https://static-cdn.jtvnw.net/previews-ttv/live_user_cmdrhughmann-{width}x{height}.jpg"
+        },
+        "channel": {
+          "mature": true,
+          "status": "COFFEE, CHAT THEN GAMEZ - !ship !twitchprime",
+          "broadcaster_language": "en",
+          "display_name": "CMDRHughMann",
+          "game": "Grand Theft Auto V",
+          "language": "en-gb",
+          "name": "cmdrhughmann",
+          "created_at": "2015-07-23T17:08:15Z",
+          "updated_at": "2017-05-01T19:36:44Z",
+          "_id": "96982484",
+          "logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/cmdrhughmann-profile_image-f97c1f6635b8e13c-300x300.jpeg",
+          "video_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/cmdrhughmann-channel_offline_image-90326b69e98cb9e7-1920x1080.png",
+          "profile_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/cmdrhughmann-profile_banner-20ecaf3a2920f8b9-480.png",
+          "profile_banner_background_color": "#000000",
+          "partner": true,
+          "url": "https://www.twitch.tv/cmdrhughmann",
+          "views": 416932,
+          "followers": 10541
+        }
+      }
+    },
+    {
+      "text": "<p>Complete your day with a shot of top-shelf awesome sauce! Kevin Pereira, Alex Corea, and the Attack crew run down the day’s biggest stories, best videos and scour the darkest corners of the interwebs to bring the stuff you’ll wish you could un-see. Come chat, submit links and voice your opinion on The Attack. <a href=\"/theattack\">We’re your family now.</a></p>\n",
+      "title": "The Attack",
+      "sponsored": false,
+      "priority": 9,
+      "scheduled": false,
+      "image": "https://s.jtvnw.net/jtv_user_pictures/hosted_images/theattack_fp_feb2017",
+      "stream": {
+        "game": "Outlast 2",
+        "viewers": 3619,
+        "video_height": 720,
+        "average_fps": 60,
+        "_id": "25183016448",
+        "delay": 0,
+        "created_at": "2017-05-01T16:49:05Z",
+        "is_playlist": false,
+        "preview": {
+          "small": "https://static-cdn.jtvnw.net/previews-ttv/live_user_theattack-80x45.jpg",
+          "medium": "https://static-cdn.jtvnw.net/previews-ttv/live_user_theattack-320x180.jpg",
+          "large": "https://static-cdn.jtvnw.net/previews-ttv/live_user_theattack-640x360.jpg",
+          "template": "https://static-cdn.jtvnw.net/previews-ttv/live_user_theattack-{width}x{height}.jpg"
+        },
+        "channel": {
+          "mature": false,
+          "status": "Ms. Steebz plays Outlast 2!",
+          "broadcaster_language": "en",
+          "display_name": "TheAttack",
+          "game": "Outlast 2",
+          "language": "en",
+          "name": "theattack",
+          "created_at": "2013-03-11T03:56:21Z",
+          "updated_at": "2017-05-01T19:36:47Z",
+          "_id": "41226075",
+          "logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/theattack-profile_image-07e2285da07ae010-300x300.png",
+          "video_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/theattack-channel_offline_image-c9015a3cfbdb2734-1920x1080.png",
+          "profile_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/theattack-profile_banner-afe6ac64bbfada61-480.png",
+          "profile_banner_background_color": "#00a7ca",
+          "partner": true,
+          "url": "https://www.twitch.tv/theattack",
+          "views": 8239740,
+          "followers": 81700
+        }
+      }
+    }
+  ]
+}
+
+window.fakeData = fakeData;
